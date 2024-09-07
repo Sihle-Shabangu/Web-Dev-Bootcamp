6e055d2c1735e55f57c1af2414151b416e7edfd5
Sihle <sshabangu061@gmail.com>
2024-09-07 16:59:28 +0200

    Calculator Done

---
+++ JavaScript/Practice/calculator.js
@@ -0,0 +1,16 @@
+function add(num1, num2){
+    return num1 + num2
+}
+function multiply(num1, num2){
+    return num1 * num2
+}
+function subtract(num1, num2){
+    return num1 - num2
+}
+function divide(num1, num2){
+    return num1 / num2
+}
+
+function calculator(num1, num2, op){
+    return op(num1, num2)
+}
